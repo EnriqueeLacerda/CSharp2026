72ff3c863cd4bbcbb7aebd701bb8c4f3c8eaf041
EnriqueeLacerda <enriquelacerda15@hotmail.com>
2026-02-11 21:21:34 -0300

    primeiro codigo do curso c#

---
+++ clodoaldo.cs
@@ -0,0 +1,12 @@
+using System; //bliblioteca do c# (system )para usar comando do console
+
+public class Program
+{
+    public static  void Main()
+    {
+        Console.WriteLine("Senai");
+    }
+}
+    
+
+
