ca908012974a1c5e24ce9a5c0c086589498dbb30
EnriqueeLacerda <enriquelacerda15@hotmail.com>
2026-02-13 19:49:20 -0300

    aula03

---
+++ LogicaDeProgramacao/Aula3/NãoSeiNada.cs
@@ -0,0 +1,8 @@
+double x, y, media;
+Console.WriteLine("digite o primeiro numero");
+x=double.Parse(Console.ReadLine());
+Console.WriteLine("digite o segundo numero");
+y=double.Parse(Console.ReadLine());
+media=(x+y)/2;
+Console.WriteLine("media="+ media);
+
