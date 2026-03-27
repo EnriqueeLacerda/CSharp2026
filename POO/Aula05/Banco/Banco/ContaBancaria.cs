92099041453a892767f4356354e2931a427c1e49
EnriqueeLacerda <enriquelacerda15@hotmail.com>
2026-03-27 18:46:28 -0300

    ok

---
+++ POO/Aula05/Banco/Banco/ContaBancaria.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace Banco
+{
+    internal class ContaBancaria
+    {
+        //Campo ou atributos
+
+        private int  numero;
+        private string titular;
+        private double saldo;
+
+
+        //construtor
+
+        public ContaBancaria(int numero, string titular)
+        {
+            this.numero = numero;
+            this.titular = titular;
+            saldo = 0;
+        }   
+                                                                         // reaproventamento de construtor            
+        public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular) // <^
+        {
+            this.saldo = saldo;
+        }
+
+        // metodos
+
+        public void Deposito(double quantia)
+        {
+            saldo += quantia;
+        }
+
+        public void Saque ( double quantia)
+        {
+            saldo -= quantia;
+        }
+
+        public void Dados()
+        {
+            Console.WriteLine("Dados da conta Cadastrada: ");
+            Console.WriteLine($" \t Titular: {titular}\n" + $" \t Numero da conta: {numero}\n " + $" \t Saldo da Conta: R$ {saldo}");
+        }
+    }
+}
---
+++ POO/Aula05/Banco/Banco/Program.cs
@@ -0,0 +1,46 @@
+﻿using Banco;
+using static System.Console;
+using static System.Globalization.CultureInfo;
+
+
+Title = "Abertura de conta";
+ContaBancaria conta;
+
+Console.ForegroundColor = ConsoleColor.Green;
+WriteLine("Abertura de conta - Nika Investmento");
+Console.ResetColor();
+
+Write("Entre com um numero de conta: ");
+int numero = int.Parse(ReadLine());
+
+Write("Entre com o nome do titular: ");
+string nome = ReadLine();
+
+Write("Deseja fazer deposito ( S / n ): ");
+char resposta = char.Parse(ReadLine().ToLower());
+
+    if (resposta == 's')
+{
+    Write("Entre com valor de deposito: R$ ");
+    double deposito = double.Parse(ReadLine());
+   conta = new ContaBancaria(numero, nome, deposito);
+}
+else
+{
+     conta = new ContaBancaria(numero, nome);
+}
+conta.Dados();
+
+Write("Entre com valor de deposito: R$ ");
+double quantia = double.Parse(ReadLine());
+conta.Deposito(quantia);
+
+Write("Entre com valor para o saque R$ ");
+quantia = double.Parse(ReadLine());
+conta.Saque(quantia);
+
+
+
+
+ReadKey();
+
