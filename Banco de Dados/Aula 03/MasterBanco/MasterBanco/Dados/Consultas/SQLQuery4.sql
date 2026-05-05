39a917ee6a1ce7077d973091f816b0d1119832c6
EnriqueeLacerda <enriquelacerda15@hotmail.com>
2026-05-04 21:52:46 -0300

    desevolvimento web aula 01

---
+++ Banco de Dados/Aula 03/MasterBanco/MasterBanco/Dados/Consultas/SQLQuery4.sql
@@ -0,0 +1,5 @@
+-- Update - Atualizar
+UPDATE Contas
+SET Titular = 'Jorginho', Numero_da_conta = 1009, Saldo = 1674.69
+WHERE Id = 4
+GO
--- Banco de Dados/Nova pasta/MasterBanco/MasterBanco/Dados/Consultas/SQLQuery3.sql
+++
@@ -1,8 +0,0 @@
-﻿-- operação read - ler --
-
-/*SELECT * FROM Contas;
-GO*/
-
-SELECT Id, Titular, Numero_da_conta,Saldo FROM Contas
-WHERE Saldo > 1000;
-GO
